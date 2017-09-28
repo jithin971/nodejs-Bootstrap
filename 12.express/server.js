cd0e07b1cce0130ca73c8baca6bdd14682cdecb8
jithin971 <jithin971@gmail.com>
2017-09-28 23:29:43 +0530

    server update

--- 12.express/server.js
+++ 12.express/server.js
@@ -1,30 +1,51 @@
-var express=require('express');
-var app=express()
-	bodyParser = require('body-parser')
-    var jsonParser = bodyParser.json();
+var express = require('express');
+var app = express()
+bodyParser = require('body-parser')
+var jsonParser = bodyParser.json();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'))
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-app.get('/',function(req,res){
-//console.log(__dirname)
+app.get('/', function (req, res) {
+    //console.log(__dirname)
 })
+var obj = [{ empNo: "10", EmpName: "jithin", Empage: 10 }, { empNo: "20", EmpName: "asd", Empage: 10 }, { empNo: "30", EmpName: "rah", Empage: 10 }]
+app.get('/employee', function (req, res) {
 
-app.get('/employee',function(req,res){
-    var obj=[{asd:"jhvj"},{asd:"jhvj"}]
     res.end(JSON.stringify(obj))
 
 })
-app.post('/employee',urlencodedParser,function(req,res){
-    console.log("params",req.body)
-    res.end("JSON.stringify(obj)")
+
+app.get('/employee/:id', function (req, res) {
+
+    myid=req.params.id;
+    obj.forEach(function (element) {
+       if(element.empNo==myid){
+         
+           res.end(JSON.stringify(element))
+
+       }
+
+    }, this);
+
+res.end("notfound")
+})
+app.post('/employee', urlencodedParser, function (req, res) {
+    console.log("params", req.body)
+    obj.push(req.body)
+    res.end("done")
 
 })
 
-var server=app.listen(8081,function(req,res){
-var host = server.address().address
-   var port = server.address()
-   
-   console.log("Example app listening at", host, port)
+var server = app.listen(8081, function (req, res) {
+    var host = server.address().address
+    var port = server.address()
+
+    console.log("Example app listening at", host, port)
 })
